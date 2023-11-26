f77a25910939679f50c9436c81d4d3962a944eb6
Samir Lekinwala <samirlekinwala@gmail.com>
2023-11-26 20:51:12 +1300

    Added frontend api to send form data

--- client/apis/fruits.ts
+++ client/apis/fruits.ts
@@ -2,8 +2,6 @@ import request from 'superagent'
 
 const rootUrl = '/api/v1'
 
-export function getFruits(): Promise<string[]> {
-  return request.get(rootUrl + '/fruits').then((res) => {
-    return res.body.fruits
-  })
+export async function postFormData(): Promise<string[]> {
+  await request.post(rootUrl + '/bought')
 }
