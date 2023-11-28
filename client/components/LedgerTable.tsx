eec1e64326dfe0d8425945748ea2efc4c794c983
Samir Lekinwala <samirlekinwala@gmail.com>
2023-11-28 23:16:52 +1300

    Working on table tailwind

--- client/components/LedgerTable.tsx
+++ client/components/LedgerTable.tsx
@@ -12,30 +12,42 @@ function LedgerTable(props: Props) {
   return (
     <div>
       <h1>The Ledger</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Date Created</th>
-            <th>Item</th>
-            <th>Price</th>
-            <th>Fees / Shipping</th>
-            <th>Platform</th>
-            <th>Bought</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((item) => (
-            <tr key={item.id}>
-              <td>{item.created_at}</td>
-              <td>{item.item}</td>
-              <td>{item.price}</td>
-              <td>{item.shipping}</td>
-              <td>{item.platform}</td>
-              <td>{item.soldOrBought}</td>
+      <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                Item
+              </th>
+              <th>Date Created</th>
+              <th>Price</th>
+              <th>Fees / Shipping</th>
+              <th>Platform</th>
+              <th>Bought</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {data.map((item) => (
+              <tr
+                key={item.id}
+                className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+              >
+                <th
+                  scope="row"
+                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                >
+                  {item.item}
+                </th>
+                <td>{item.created_at}</td>
+                <td>{item.price}</td>
+                <td>{item.shipping}</td>
+                <td>{item.platform}</td>
+                <td>{item.soldOrBought}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   )
 }
