b43a4ac3d14bfd5b7825d7aaaa6a6d946e7face0
Samir Lekinwala <samirlekinwala@gmail.com>
2024-05-02 12:00:25 +1200

    Remove redundant list items in burgermenu

--- client/components/BurgerMenu.tsx
+++ client/components/BurgerMenu.tsx
@@ -129,34 +129,18 @@ import {
   ListItemPrefix,
   ListItemSuffix,
   Chip,
-  Accordion,
-  AccordionHeader,
-  AccordionBody,
-  Alert,
-  Input,
   Drawer,
   Card,
   ThemeProvider,
 } from '@material-tailwind/react'
 import {
-  PresentationChartBarIcon,
-  ShoppingBagIcon,
   UserCircleIcon,
   Cog6ToothIcon,
   InboxIcon,
   PowerIcon,
 } from '@heroicons/react/24/solid'
-import {
-  ChevronRightIcon,
-  ChevronDownIcon,
-  CubeTransparentIcon,
-  MagnifyingGlassIcon,
-  Bars3Icon,
-  XMarkIcon,
-} from '@heroicons/react/24/outline'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import { Link } from 'react-router-dom'
-import { getAllItems } from '../apis/fruits'
-import { ModuleNamespace } from 'vite/types/hot.js'
 import * as models from '../models/items.tsx'
 
 interface Props {
@@ -170,15 +154,6 @@ export function BurgerMenu(props: Props) {
   // const [openAlert, setOpenAlert] = React.useState(true)
   const [isDrawerOpen, setIsDrawerOpen] = React.useState(false)
 
-  // const [data, setData] = useState<models.item[] | null>(null)
-
-  // async function getResponse() {
-  //   const response = await getAllItems()
-  //   const data = response // Extracting data as a JSON Object from the response
-  //   // console.log(data.body)
-  //   return setData(data.body)
-  // }
-
   function numberOfItemsInInventory() {
     return data.filter((item) => item.soldOrBought == 'bought').length
   }
@@ -187,9 +162,6 @@ export function BurgerMenu(props: Props) {
   }
 
   console.log(numberOfItemsInInventory())
-  // getResponse()
-  // const data = getData()
-  // console.log('test', data())
 
   const customTheme = {
     ...{
@@ -208,9 +180,9 @@ export function BurgerMenu(props: Props) {
     },
   }
 
-  const handleOpen = (value) => {
-    setOpen(open === value ? 0 : value)
-  }
+  // const handleOpen = (value) => {
+  //   setOpen(open === value ? 0 : value)
+  // }
 
   const openDrawer = () => setIsDrawerOpen(true)
   const closeDrawer = () => setIsDrawerOpen(false)
@@ -248,94 +220,6 @@ export function BurgerMenu(props: Props) {
             />
           </div> */}
             <List>
-              {/* <Accordion
-              open={open === 1}
-              icon={
-                <ChevronDownIcon
-                  strokeWidth={2.5}
-                  className={`mx-auto h-4 w-4 transition-transform ${
-                    open === 1 ? 'rotate-180' : ''
-                  }`}
-                />
-              }
-            >
-              <ListItem className="p-0" selected={open === 1}>
-                <AccordionHeader
-                  onClick={() => handleOpen(1)}
-                  className="border-b-0 p-3"
-                >
-                  <ListItemPrefix>
-                    <PresentationChartBarIcon className="h-5 w-5" />
-                  </ListItemPrefix>
-                  <Typography color="blue-gray" className="mr-auto font-normal">
-                    Dashboard
-                  </Typography>
-                </AccordionHeader>
-              </ListItem>
-              <AccordionBody className="py-1">
-                <List className="p-0">
-                  <ListItem>
-                    <ListItemPrefix>
-                      <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
-                    </ListItemPrefix>
-                    Analytics
-                  </ListItem>
-                  <ListItem>
-                    <ListItemPrefix>
-                      <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
-                    </ListItemPrefix>
-                    Reporting
-                  </ListItem>
-                  <ListItem>
-                    <ListItemPrefix>
-                      <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
-                    </ListItemPrefix>
-                    Projects
-                  </ListItem>
-                </List>
-              </AccordionBody>
-            </Accordion>
-            <Accordion
-              open={open === 2}
-              icon={
-                <ChevronDownIcon
-                  strokeWidth={2.5}
-                  className={`mx-auto h-4 w-4 transition-transform ${
-                    open === 2 ? 'rotate-180' : ''
-                  }`}
-                />
-              }
-            >
-              <ListItem className="p-0" selected={open === 2}>
-                <AccordionHeader
-                  onClick={() => handleOpen(2)}
-                  className="border-b-0 p-3"
-                >
-                  <ListItemPrefix>
-                    <ShoppingBagIcon className="h-5 w-5" />
-                  </ListItemPrefix>
-                  <Typography color="blue-gray" className="mr-auto font-normal">
-                    E-Commerce
-                  </Typography>
-                </AccordionHeader>
-              </ListItem>
-              <AccordionBody className="py-1">
-                <List className="p-0">
-                  <ListItem>
-                    <ListItemPrefix>
-                      <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
-                    </ListItemPrefix>
-                    Orders
-                  </ListItem>
-                  <ListItem>
-                    <ListItemPrefix>
-                      <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
-                    </ListItemPrefix>
-                    Products
-                  </ListItem>
-                </List>
-              </AccordionBody>
-            </Accordion> */}
               <hr className="my-2 border-blue-gray-50" />
               <ListItem>
                 <ListItemPrefix>
@@ -371,7 +255,7 @@ export function BurgerMenu(props: Props) {
                   />
                 </ListItemSuffix>
               </ListItem>
-              <ListItem>
+              {/* <ListItem>
                 <ListItemPrefix>
                   <UserCircleIcon className="h-5 w-5" />
                 </ListItemPrefix>
@@ -388,41 +272,8 @@ export function BurgerMenu(props: Props) {
                   <PowerIcon className="h-5 w-5" />
                 </ListItemPrefix>
                 Log Out
-              </ListItem>
+              </ListItem> */}
             </List>
-            {/* <Alert
-            open={openAlert}
-            className="mt-auto"
-            onClose={() => setOpenAlert(false)}
-          >
-            <CubeTransparentIcon className="mb-4 h-12 w-12" />
-            <Typography variant="h6" className="mb-1">
-              Upgrade to PRO
-            </Typography>
-            <Typography variant="small" className="font-normal opacity-80">
-              Upgrade to Material Tailwind PRO and get even more components,
-              plugins, advanced features and premium.
-            </Typography>
-            <div className="mt-4 flex gap-3">
-              <Typography
-                as="a"
-                href="#"
-                variant="small"
-                className="font-medium opacity-80"
-                onClick={() => setOpenAlert(false)}
-              >
-                Dismiss
-              </Typography>
-              <Typography
-                as="a"
-                href="#"
-                variant="small"
-                className="font-medium"
-              >
-                Upgrade Now
-              </Typography>
-            </div>
-          </Alert> */}
           </Card>
         </Drawer>
       </ThemeProvider>
