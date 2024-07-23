039b65512c46f632553b4d297a4421b5d2abe08e
Samir Lekinwala <samirlekinwala@gmail.com>
2024-07-23 14:14:05 +1200

    comment out tooltip script from index.html and remove from vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from 'tailwindcss'
+// import '/node_modules/@material-tailwind/html/scripts/tooltip.js'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,11 +13,11 @@ export default defineConfig({
       '/api': 'http://localhost:3000',
     },
   },
-  build: {
-    rollupOptions: {
-      external: [
-        './node_modules/@material-tailwind/html@latest/scripts/tooltip.js',
-      ],
-    },
-  },
+  // build: {
+  //   rollupOptions: {
+  //     external: [
+  //       '/node_modules/@material-tailwind/html@latest/scripts/tooltip.js',
+  //     ],
+  //   },
+  // },
 })
